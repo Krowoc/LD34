55bf2964fcd9ae9afe8564cea2f7f85a2c60b1c6
Adam Silcott <adsilcott@gmail.com>
2015-12-12 12:34:39 -0500

    Removing zoom from camera and adding character leading

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,9 +5,13 @@ public class CameraControl : MonoBehaviour {
 
 	[SerializeField]
 	GameObject followObject;
+	Rigidbody2D followRBody;
 
 	[SerializeField]
 	float followSpeed;
+
+	[SerializeField]
+	float leadDistance = 3.0f;
 
 	float zPosition;
 
@@ -15,15 +19,19 @@ public class CameraControl : MonoBehaviour {
 	void Start () {
 
 		zPosition = transform.position.z;
+		followRBody = followObject.GetComponent<Rigidbody2D>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 newPosition = Vector3.Lerp(transform.position, followObject.transform.position, Time.deltaTime * followSpeed);
-		newPosition.z = Mathf.Lerp(transform.position.z, followObject.transform.localScale.x * -10.0f, Time.deltaTime * followSpeed);
+		Vector3 followPosition = followObject.transform.position;
+		followPosition.x += followRBody.velocity.x * leadDistance;
 
+		Vector3 newPosition = Vector3.Lerp(transform.position, followPosition, Time.deltaTime * followSpeed);
+		newPosition.z = zPosition;//Mathf.Lerp(transform.position.z, followObject.transform.localScale.x * -10.0f, Time.deltaTime * followSpeed);
+		
 		transform.position = newPosition;
 		
 	}
