596c0414e2af238d8aea3ef077476cf11662cbff
Adam Silcott <adsilcott@gmail.com>
2015-12-12 10:35:36 -0500

    Re-adding prototype assets

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	Rigidbody2D rBody;
+
+	[SerializeField]
+	float maximumScale = 1.0f;
+
+	[SerializeField]
+	float minimumScale = 0.25f;
+
+	[SerializeField]
+	float scaleSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Z))
+		{
+			float newScale = transform.localScale.x;
+			newScale += scaleSpeed;
+			if (newScale > maximumScale)
+				newScale = maximumScale;
+
+			transform.localScale = new Vector3(newScale, newScale, 0.0f); ;
+
+		}
+
+		if (Input.GetKey(KeyCode.X))
+		{
+			float newScale = transform.localScale.x;
+			newScale -= scaleSpeed;
+			if (newScale < minimumScale)
+				newScale = minimumScale;
+
+			transform.localScale = new Vector3(newScale, newScale, 0.0f); ;
+
+		}
+	}
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	[SerializeField]
+	GameObject followObject;
+
+	[SerializeField]
+	float followSpeed;
+
+	float zPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		zPosition = transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 newPosition = Vector3.Lerp(transform.position, followObject.transform.position, Time.deltaTime * followSpeed);
+		newPosition.z = Mathf.Lerp(transform.position.z, followObject.transform.localScale.x * -10.0f, Time.deltaTime * followSpeed);
+
+		transform.position = newPosition;
+		
+	}
+}
