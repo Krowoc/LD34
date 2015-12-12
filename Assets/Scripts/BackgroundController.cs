72750d6e8e940cdb9f9d2c8648ab5c99b0ef1495
Adam Silcott <adsilcott@gmail.com>
2015-12-12 17:31:54 -0500

    Background-- broken

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	[SerializeField]
+	float scrollSpeed = 0.0005f;
+
+	[SerializeField]
+	float scrollDistance = 16.0f;
+
+	[SerializeField]
+	float startingPosition;
+	float offset = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		offset += scrollSpeed;
+		if (offset >= scrollDistance)
+		{
+			offset = scrollDistance;
+		}
+
+		Vector3 newPosition = transform.position;
+
+		newPosition.y = startingPosition + offset;
+
+		transform.position = newPosition;
+	}
+}
