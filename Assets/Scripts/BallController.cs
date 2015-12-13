8f0ed436bb086ae87c6d047f96989d7b676c841b
Adam Silcott <adsilcott@gmail.com>
2015-12-12 19:24:08 -0500

    Scaling

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BallController : MonoBehaviour {
 
 	//Rigidbody2D rBody;
+	SkinnedMeshRenderer shapes;
 
 	[SerializeField]
 	float maximumScale = 1.0f;
@@ -17,7 +18,7 @@ public class BallController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//rBody = GetComponent<Rigidbody2D>();
-		
+		shapes = GetComponentInChildren<SkinnedMeshRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -30,7 +31,7 @@ public class BallController : MonoBehaviour {
 				newScale = maximumScale;
 
 			transform.localScale = new Vector3(newScale, newScale, newScale);
-
+			Animate(newScale);
 		}
 
 		if (Input.GetKey(KeyCode.X))
@@ -41,7 +42,21 @@ public class BallController : MonoBehaviour {
 				newScale = minimumScale;
 
 			transform.localScale = new Vector3(newScale, newScale, newScale);
-
+			Animate(newScale);
 		}
+	}
+
+	void Animate(float scale)
+	{
+		Debug.Log(scale);
+		float s = Mathf.InverseLerp(minimumScale, maximumScale, scale);
+
+		//Debug.Log(s);
+		s = (s * 200.0f) - 125.0f;
+
+		if (s > 0)
+			shapes.SetBlendShapeWeight(1, s);
+		else
+			shapes.SetBlendShapeWeight(0, -s);
 	}
 }
