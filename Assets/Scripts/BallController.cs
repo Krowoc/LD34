f5513c28f9521312c26ed6ba8e3fe8fec4b88e4f
Adam Silcott <adsilcott@gmail.com>
2015-12-12 23:57:05 -0500

    Adding Jumping and other tweaks

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -14,22 +14,46 @@ public class BallController : MonoBehaviour {
 	[SerializeField]
 	float scaleSpeed = 0.1f;
 
+	[SerializeField]
+	float height = 1.0f;
+
+
+	Rigidbody rBody;
+	[SerializeField]
+	bool onGround = false;
+
 	// Use this for initialization
 	void Start () {
 		shapes = GetComponentInChildren<SkinnedMeshRenderer>();
+		rBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		Ray ray = new Ray(transform.position, Vector3.down);
+		RaycastHit[] hits = Physics.RaycastAll(ray, height);
+
+		if (hits.Length > 0)
+			onGround = true;
+		else
+			onGround = false;
+
+
 		if(Input.GetKey(KeyCode.Z))
 		{
 			float newScale = transform.localScale.x;
+
+			if (onGround && newScale == minimumScale)
+				rBody.AddForce(new Vector3(9f, 9f, 0f), ForceMode.Impulse); //Jump
+
 			newScale += scaleSpeed;
 			if (newScale > maximumScale)
 				newScale = maximumScale;
-
+			
 			transform.localScale = new Vector3(newScale, newScale, newScale);
 			Animate(newScale);
+			
 		}
 
 		if (Input.GetKey(KeyCode.X))
