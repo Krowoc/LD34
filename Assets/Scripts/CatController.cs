002f5ed152cf97dd517ee972a94610f8f9c84a33
Adam Silcott <adsilcott@gmail.com>
2015-12-13 23:39:52 -0500

    Fixing cats

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class CatController : MonoBehaviour {
 
+	Animator anim;
+	Rigidbody rBody;
+
+	[SerializeField]
+	float collisionForce = 3.0f;
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		anim = GetComponentInChildren<Animator>();
+		rBody = GetComponentInChildren<Rigidbody>();
+		rBody.detectCollisions = false;
+		rBody.useGravity = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void Pounce()
+	{
+		anim.SetTrigger("Pounce");
+
+	}
+
+	public void Death()
+	{
+		StartCoroutine(DeathCoroutine());
+	}
+
+	IEnumerator DeathCoroutine()
+	{
+		anim.SetTrigger("Collide");
+		rBody.gameObject.transform.SetParent(null);
+		rBody.detectCollisions = true;
+		rBody.useGravity = true;
+		rBody.AddForce(collisionForce, 0f, collisionForce, ForceMode.VelocityChange);
+		
+
+		yield return new WaitForSeconds(5.0f);
+
+		GameObject.Destroy(gameObject);
 	}
 }
