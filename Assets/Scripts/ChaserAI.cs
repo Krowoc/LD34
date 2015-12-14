cf9dc0ca4a0c19896cc82a729339db56e5cbc05b
Jesse Smith <jesse.smth09@gmail.com>
2015-12-14 03:03:37 -0500

    Added chef model to chasingAI

--- Assets/Scripts/ChaserAI.cs
+++ Assets/Scripts/ChaserAI.cs
@@ -23,22 +23,28 @@ public class ChaserAI : MonoBehaviour
     [SerializeField]
     private GameObject chaser;
     private Rigidbody rbAI;
+    private Animator chaserAnimation;
     
  
 
     void Start()
     {
         rbAI = chaser.GetComponent<Rigidbody>();
+        chaserAnimation = chaser.GetComponent<Animator>();
         transform.LookAt(target.transform);
+        chaserAnimation.SetTrigger("Chasing");
     }
 
     void FixedUpdate()
     {
         checkIfGrounded();
+        
 
         if (following)
         {
+            //Chaser needs to have interpolate ON in the rigidbody for a smooth transition
             rbAI.MovePosition(Vector3.MoveTowards(chaser.transform.position, target.transform.position, moveSpeed * Time.deltaTime));
+ 
         }
 
         //Pushes the object down so, it doesn't move with the targer upward
@@ -66,7 +72,6 @@ public class ChaserAI : MonoBehaviour
     }
 
 
-
     //If the object gets the target. If the object is blocked by a tree than a respawn occurs. 
     void OnCollisionEnter(Collision col)
     {
@@ -74,13 +79,15 @@ public class ChaserAI : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             //Death method start here
-            Debug.Log("Fisherman catches fish");
+            Debug.Log("Chef catches fish");
+            chaserAnimation.SetTrigger("KnifeWave");
         }
 
         //Respawns the chaser
         else if (col.gameObject.tag == "Tree")
         {
             rbAI.velocity = Vector3.zero;
+            chaserAnimation.SetTrigger("Idle");
             following = false;
             StartCoroutine(Respawn());
         }
@@ -96,6 +103,8 @@ public class ChaserAI : MonoBehaviour
         Vector3 respawnPositon = new Vector3(target.transform.position.x, target.transform.position.y + yOffset);
         rbAI.position = respawnPositon;
         following = true;
+        chaserAnimation.SetTrigger("Chasing");
+        //transform.LookAt(target.transform);
 
     }
        
