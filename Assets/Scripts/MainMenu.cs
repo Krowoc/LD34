9a58ff34c2e1cfd37e4e3621df30b86f800b9eee
Ian Lane <islane@rocketmail.com>
2015-12-14 19:02:57 -0500

    Added MainMenu script (untested), need to modify later or update to use these scene names

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+ 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+                Application.Quit();
+            else
+                OnBackButton();
+        }
+    }
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("MainScene001");
+    }
+
+    public void OnCreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
